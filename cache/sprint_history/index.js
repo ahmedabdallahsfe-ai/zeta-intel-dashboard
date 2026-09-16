7d009e5eb0f00acedd94a22d3962af21b172bd8f
Zeta Intel Dashboard <zeta@dashboard>
2026-09-17 02:08:16 +0300

    Update sprint cache and dashboard asset versioning

--- cache/sprint_history/index.js
+++ cache/sprint_history/index.js
@@ -1,1 +1,1 @@
-window.SPRINT_HISTORY_INDEX = [{"key":"2026-07","name":"July","file":"cache/sprint_history/sprint_2026-07.data.js","generatedAt":"2026-09-16 14:59:59"}];
+window.SPRINT_HISTORY_INDEX = [{"key":"2026-07","name":"July","file":"cache/sprint_history/sprint_2026-07.data.js","generatedAt":"2026-09-16 22:58:03"}];
